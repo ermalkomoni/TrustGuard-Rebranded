13acc39c5d48a21f35a66577679bc9909c6d9cfb
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 02:31:03 +0100

    create method for InsuranceType

--- TrustGuard_API/Controllers/InsuranceTypeController.cs
+++ TrustGuard_API/Controllers/InsuranceTypeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using TrustGuard_API.Data;
+using TrustGuard_API.DTOs;
 using TrustGuard_API.Models;
 
 namespace TrustGuard_API.Controllers;
@@ -49,5 +50,57 @@ public class InsuranceTypeController : ControllerBase
         _response.Result = insuranceType;
         _response.StatusCode = HttpStatusCode.OK;
         return Ok(_response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ApiResponse>> CreateInsuranceType([FromForm] InsuranceTypeCreateDTO insuranceTypeCreateDto)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (insuranceTypeCreateDto.File == null || insuranceTypeCreateDto.File.Length == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    return BadRequest();
+                }
+
+                // Convert the image file to base64
+                using (var stream = new MemoryStream())
+                {
+                    await insuranceTypeCreateDto.File.CopyToAsync(stream);
+                    var imageBytes = stream.ToArray();
+                    var base64Image = Convert.ToBase64String(imageBytes);
+
+                    InsuranceType menuItemToCreate = new()
+                    {
+                        Name = insuranceTypeCreateDto.Name,
+                        Price = insuranceTypeCreateDto.Price,
+                        Category = insuranceTypeCreateDto.Category,
+                        SpecialTag = insuranceTypeCreateDto.SpecialTag,
+                        Description = insuranceTypeCreateDto.Description,
+                        Image = base64Image // Store the image as base64 string
+                    };
+
+                    _db.InsuranceTypes.Add(menuItemToCreate);
+                    _db.SaveChanges();
+                    _response.Result = menuItemToCreate;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem", new { id = menuItemToCreate.Id }, _response);
+                }
+            }
+            else
+            {
+                _response.IsSuccess = false;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string>() { ex.ToString() };
+        }
+        return _response;
     }
 }
