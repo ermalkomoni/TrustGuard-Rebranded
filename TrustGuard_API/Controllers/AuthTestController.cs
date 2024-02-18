a77255ed782df6c430265cbba42c1031a0f9087f
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 16:48:06 +0100

    testing authentication

---
+++ TrustGuard_API/Controllers/AuthTestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrustGuard_API.Utility;
+
+namespace TrustGuard_API.Controllers;
+
+[Route("api/AuthTest")]
+[ApiController]
+public class AuthTestController : Controller
+{
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<string>> GetSomething()
+    {
+        return "You are authenticated";
+    }
+
+    [HttpGet("{id:int}")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public async Task<ActionResult<string>> GetSomething(int someIntValue)
+    {
+        //authorization -> Authentication + Some access/roles
+        return "You are Authorized with Role of Admin";
+    }
+}
