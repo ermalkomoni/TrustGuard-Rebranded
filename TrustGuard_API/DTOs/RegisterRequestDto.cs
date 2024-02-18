5ccf8e984f517fbe77edb3324568d96246adb6b9
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 22:50:41 +0100

    adding RegisterRequestDTO

---
+++ TrustGuard_API/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrustGuard_API.DTOs
+{
+    public class RegisterRequestDTO
+    {
+        public string PersonalId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
