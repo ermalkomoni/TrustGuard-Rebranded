638e62bc861af7ac5a0feac92a234f5bdc638884
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 10:58:13 +0100

    adding GetOrders method

---
+++ TrustGuard_API/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Text.Json;
+using TrustGuard_API.Data;
+using TrustGuard_API.Models;
+
+namespace TrustGuard_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetOrders(string? userId,
+            string? searchString, string? status, int pageNumber = 1, int pageSize = 5)
+        {
+            try
+            {
+                IEnumerable<OrderHeader> orderHeaders =
+                    _db.OrderHeaders.Include(u => u.OrderDetails)
+                        .ThenInclude(u => u.InsuranceType)
+                        .OrderByDescending(u => u.OrderHeaderId);
+
+
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    orderHeaders = orderHeaders.Where(u => u.ApplicationUserId == userId);
+                }
+
+                if (!string.IsNullOrEmpty(searchString))
+                {
+                    orderHeaders = orderHeaders
+                        .Where(u => u.PickupPhoneNumber.ToLower().Contains(searchString.ToLower()) ||
+                                    u.PickupEmail.ToLower().Contains(searchString.ToLower())
+                                    || u.PickupName.ToLower().Contains(searchString.ToLower()));
+                }
+                if (!string.IsNullOrEmpty(status))
+                {
+                    orderHeaders = orderHeaders.Where(u => u.Status.ToLower() == status.ToLower());
+                }
+
+                Pagination pagination = new()
+                {
+                    CurrentPage = pageNumber,
+                    PageSize = pageSize,
+                    TotalRecords = orderHeaders.Count(),
+                };
+
+                Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagination));
+
+                _response.Result = orderHeaders.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+
+}
