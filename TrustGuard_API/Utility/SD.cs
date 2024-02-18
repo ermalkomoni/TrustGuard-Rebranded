6114fc8795f3e18ed1ee36c407c843bebf720603
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 12:25:33 +0100

    adding Static Details

---
+++ TrustGuard_API/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿namespace TrustGuard_API.Utility
+{
+    public class SD
+    {
+        public const string status_pending = "Pending";
+        public const string status_confirmed = "Confirmed";
+        public const string status_inProgress = "In progress";
+        public const string status_approved = "Approved";
+        public const string status_Completed = "Completed";
+        public const string status_Cancelled = "Cancelled";
+    }
+}
