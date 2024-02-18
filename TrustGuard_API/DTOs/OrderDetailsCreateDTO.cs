16928983c05f73a013a5086c67382cea865004c8
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 12:25:21 +0100

    adding DTOs needed for CreateOrder

---
+++ TrustGuard_API/DTOs/OrderDetailsCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrustGuard_API.DTOs
+{
+    public class OrderDetailsCreateDTO
+    {
+        [Required]
+        public int InsuranceTypeId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
