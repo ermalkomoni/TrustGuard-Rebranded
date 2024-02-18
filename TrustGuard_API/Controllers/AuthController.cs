6386de380307f562515c28b60747628c313f0153
Ermalkomoni <ermalkk14@gmail.com>
2024-02-18 22:51:10 +0100

    adding user registration method with roles

--- TrustGuard_API/Controllers/AuthController.cs
+++ TrustGuard_API/Controllers/AuthController.cs
@@ -87,6 +87,66 @@ public class AuthController : ControllerBase
         _response.IsSuccess = true;
         _response.Result = loginResponse;
         return Ok(_response);
+    }
 
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
+    {
+        ApplicationUser userFromDb = _db.ApplicationUsers
+            .FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+
+        if (userFromDb != null)
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username already exists");
+            return BadRequest(_response);
+        }
+
+        ApplicationUser newUser = new()
+        {
+            UserName = model.UserName,
+            Email = model.UserName,
+            NormalizedEmail = model.UserName.ToUpper(),
+            FullName = model.FullName,
+            PersonalId = model.PersonalId
+        };
+
+        try
+        {
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+            if (result.Succeeded)
+            {
+                if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                {
+                    //create roles in database
+                    await _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                    await _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+                }
+
+                if (model.Role.ToLower() == SD.Role_Admin)
+
+                {
+                    await _userManager.AddToRoleAsync(newUser, SD.Role_Admin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+        _response.StatusCode = HttpStatusCode.BadRequest;
+        _response.IsSuccess = false;
+        _response.ErrorMessages.Add("Error while registering");
+        return BadRequest(_response);
     }
 }
