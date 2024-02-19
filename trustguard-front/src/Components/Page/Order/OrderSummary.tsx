d64ddc32c806f8ec6e779fe0d77a1498a31345f1
Ermalkomoni <ermalkk14@gmail.com>
2024-02-19 14:41:47 +0100

    adding orderSummary

---
+++ trustguard-front/src/Components/Page/Order/OrderSummary.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { getStatusColor } from "../../../Helper";
+import { cartItemModel } from "../../../Interfaces";
+import { orderSummaryProps } from "./orderSummaryProps";
+import { useNavigate } from "react-router-dom";
+import { SD_Roles, SD_Status } from "../../../Utility/SD";
+import { RootState } from "../../../Storage/Redux/store";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useUpdateOrderHeaderMutation } from "../../../Apis/orderApi";
+import { MainLoader } from "../Common";
+
+function OrderSummary({ data, userInput }: orderSummaryProps) {
+  const badgeTypeColor = getStatusColor(data.status!);
+  const navigate = useNavigate();
+  const userData = useSelector((state: RootState) => state.userAuthStore);
+  const [loading, setIsLoading] = useState(false);
+  const [updateOrderHeader] = useUpdateOrderHeaderMutation();
+  
+  const nextStatus: any =
+    data.status! === SD_Status.CONFIRMED
+      ? { color: "info", value: SD_Status.IN_PROGRESS }
+      : data.status! === SD_Status.IN_PROGRESS
+      ? { color: "warning", value: SD_Status.APPROVED }
+      : data.status! === SD_Status.APPROVED && {
+          color: "success",
+          value: SD_Status.COMPLETED,
+        };
+
+  const handleNextStatus = async () => {
+    setIsLoading(true);
+    console.log(data);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: nextStatus.value,
+    });
+
+    setIsLoading(false);
+  };
+
+  const handleCancel = async () => {
+    setIsLoading(true);
+    await updateOrderHeader({
+      orderHeaderId: data.id,
+      status: SD_Status.CANCELLED,
+    });
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      {loading && <MainLoader />}
+      {!loading && (
+        <>
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="text-success">Order Summary</h3>
+            <span className={`btn btn-outline-${badgeTypeColor} fs-6`}>
+              {data.status}
+            </span>
+          </div>
+          <div className="mt-3">
+            <div className="border py-3 px-2">Name : {userInput.name}</div>
+            <div className="border py-3 px-2">Email : {userInput.email}</div>
+            <div className="border py-3 px-2">
+              Phone : {userInput.phoneNumber}
+            </div>
+            <div className="border py-3 px-2">
+              <h4 className="text-success">Insurances</h4>
+              <div className="p-3">
+                {data.cartItems?.map(
+                  (cartItem: cartItemModel, index: number) => {
+                    return (
+                      <div className="d-flex" key={index}>
+                        <div className="d-flex w-100 justify-content-between">
+                          <p>{cartItem.insuranceType?.name}</p>
+                          <p>
+                            ${cartItem.insuranceType?.price} x {cartItem.quantity} =
+                          </p>
+                        </div>
+                        <p style={{ width: "70px", textAlign: "right" }}>
+                          $
+                          {(cartItem.insuranceType?.price ?? 0) *
+                            (cartItem.quantity ?? 0)}
+                        </p>
+                      </div>
+                    );
+                  }
+                )}
+
+                <hr />
+                <h4 className="text-danger" style={{ textAlign: "right" }}>
+                  ${data.cartTotal?.toFixed(2)}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-between align-items-center mt-3">
+            <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+              Back to Orders
+            </button>
+            {userData.role == SD_Roles.ADMIN && (
+              <div className="d-flex">
+                {data.status! !== SD_Status.CANCELLED &&
+                  data.status! !== SD_Status.COMPLETED && (
+                    <button
+                      className="btn btn-danger mx-2"
+                      onClick={handleCancel}
+                    >
+                      Cancel
+                    </button>
+                  )}
+                <button
+                  className={`btn btn-${nextStatus.color}`}
+                  onClick={handleNextStatus}
+                >
+                  {nextStatus.value}
+                </button>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default OrderSummary;
