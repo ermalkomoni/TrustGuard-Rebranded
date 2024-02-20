518d4d1ecb0f1404ea60d9c33446dbb5196a6440
Ermalkomoni <ermalkk14@gmail.com>
2024-02-20 12:13:26 +0100

    adding pages

---
+++ trustguard-front/src/Pages/InsuranceTypeDetails.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetInsuranceTypeByIdQuery } from "../Apis/insuranceTypeApi";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useUpdateShoppingCartMutation } from "../Apis/shoppingCartApi";
+import { MainLoader, MiniLoader } from "../Components/Page/Common";
+import { apiResponse, userModel } from "../Interfaces";
+import { toastNotify } from "../Helper";
+import { RootState } from "../Storage/Redux/store";
+import { useSelector } from "react-redux";
+
+function InsuranceTypeDetails() {
+  const { insuranceTypeId } = useParams();
+  const { data, isLoading } = useGetInsuranceTypeByIdQuery(insuranceTypeId);
+  const navigate = useNavigate();
+  const [quantity, setQuantity] = useState(1);
+  const [isAddingToCart, setIsAddingToCart] = useState<boolean>(false);
+  const [updateShoppingCart] = useUpdateShoppingCartMutation();
+  const userData: userModel = useSelector(
+    (state: RootState) => state.userAuthStore
+  );
+
+  const handleQuantity = (counter: number) => {
+    let newQuantity = quantity + counter;
+    if (newQuantity == 0) {
+      newQuantity = 1;
+    }
+    setQuantity(newQuantity);
+    return;
+  };
+
+  const handleAddToCart = async (insuranceTypeId: number) => {
+    if (!userData.id) {
+      navigate("/login");
+      return;
+    }
+    setIsAddingToCart(true);
+    const response: apiResponse = await updateShoppingCart({
+      insuranceTypeId: insuranceTypeId,
+      updateQuantityBy: quantity,
+      userId: userData.id,
+    });
+
+    if (response.data && response.data.isSuccess) {
+      toastNotify("Item added to cart successfully!");
+    }
+    setIsAddingToCart(false);
+  };
+
+  return (
+    <div className="container pt-4 pt-md-5">
+      {!isLoading ? (
+        <div className="row">
+          <div className="col-7">
+            <h2 className="text-success">{data.result?.name}</h2>
+            <span>
+              <span
+                className="badge text-bg-dark pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.category}
+              </span>
+            </span>
+            <span>
+              <span
+                className="badge text-bg-light pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.specialTag}
+              </span>
+            </span>
+            <p style={{ fontSize: "20px" }} className="pt-2">
+              {data.result?.description}
+            </p>
+            <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
+            <span
+              className="pb-2  p-3"
+              style={{ border: "1px solid #333", borderRadius: "30px" }}
+            >
+              <i
+                onClick={() => {
+                  handleQuantity(-1);
+                }}
+                className="bi bi-dash p-1"
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+              <span className="h3 mt-3 px-3">{quantity}</span>
+              <i
+                className="bi bi-plus p-1"
+                onClick={() => {
+                  handleQuantity(+1);
+                }}
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+            </span>
+            <div className="row pt-4">
+              <div className="col-5">
+                {isAddingToCart ? (
+                  <button disabled className="btn btn-success form-control">
+                    <MiniLoader />
+                  </button>
+                ) : (
+                  <button style={{backgroundColor: '#F87C2D', color: '#fff'}}
+                    className="btn form-control"
+                    onClick={() => handleAddToCart(data.result?.id)}
+                  >
+                    Add to Cart
+                  </button>
+                )}
+              </div>
+
+              <div className="col-5">
+                <button
+                  className="btn btn-secondary form-control"
+                  onClick={() => navigate(-1)}
+                >
+                  Back to Home
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-5">
+            <img
+              src={`data:image/png;base64,${data.result.image}`}
+              width="100%"
+              // style={{ borderRadius: "50%" }}
+              alt="No content"
+            ></img>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="d-flex justify-content-center"
+          style={{ width: "100%" }}
+        >
+          <MainLoader />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default InsuranceTypeDetails;
---
+++ trustguard-front/src/Pages/Payment.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useLocation } from "react-router";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
+import { OrderSummary } from "../Components/Page/Order";
+
+function Payment() {
+  const {
+    state: { apiResult, userInput },
+  } = useLocation();
+  const stripePromise = loadStripe(
+    "pk_test_51NlcUaDyljjFEO3CvODJGxJcLfuQ3MQuG7UU29enwNtXBTdj9KIpgTjlmJjSvU4Mr2Oj88trKJV0Riss7DMMWAT900NRflZp7o"
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <div className="container m-5 p-5">
+        <div className="row">
+          <div className="col-md-7">
+            <OrderSummary data={apiResult} userInput={userInput} />
+          </div>
+          <div className="col-md-4 offset-md-1">
+            <h3 className="text-success">Payment</h3>
+            <div className="mt-5">
+              <PaymentForm data={apiResult} userInput={userInput} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Elements>
+  );
+}
+
+export default Payment;
