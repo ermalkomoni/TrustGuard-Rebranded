f601fd6be38d5653bb0d15a32ad8288fdd680ff0
Ermalkomoni <ermalkk14@gmail.com>
2024-02-20 12:13:39 +0100

    adding SD (static details)

---
+++ trustguard-front/src/Utility/SD.ts
@@ -0,0 +1,26 @@
+export enum SD_Roles {
+  ADMIN = "admin",
+  CUTOMER = "customer",
+}
+
+export enum SD_Status {
+  PENDING = "Pending",
+  CONFIRMED = "Confirmed",
+  IN_PROGRESS = "In Progress",
+  APPROVED = "Approved",
+  COMPLETED = "Completed",
+  CANCELLED = "Cancelled",
+}
+
+export enum SD_Categories {
+  CAR = "Car",
+  PROPERTY = "Property",
+  INDIVIDUAL = "Individual",
+}
+
+export enum SD_SortTypes {
+  PRICE_LOW_HIGH = "Price Low - High",
+  PRICE_HIGH_LOW = "Price High - Low",
+  NAME_A_Z = "Name A - Z",
+  NAME_Z_A = "Name Z - A",
+}
